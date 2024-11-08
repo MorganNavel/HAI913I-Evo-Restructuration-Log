d8aea65896292d5bcb60fc07baefc8e081ef02ab
Éric GILLES <eric.gilles@etu.umontpellier.fr>
2024-11-08 14:33:05 +0100

    update TP2 Cli & Utils

--- TP2/TP2-JDT/src/main/java/cli/Cli.java
+++ TP2/TP2-JDT/src/main/java/cli/Cli.java
@@ -1,10 +1,7 @@
 package cli;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
